943fc441908f3bdde08d2a2dcab665257a6bd76e
Jopolo-15 <jcamcam15@gmail.com>
2019-11-22 23:15:39 +0800

    gui

---
+++ Form1.vb
@@ -0,0 +1,9 @@
+﻿Public Class Form1
+    Private Sub Button29_Click(sender As Object, e As EventArgs) Handles Button29.Click
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
